3e6cf8ebb4bb2beee745b3b10db26f9dba62400f
Manul Aggarwal <manul.agg333@gmail.com>
2018-12-02 18:46:35 +0100

    [DEV] Added Providers logic to get the jdbc driver, implementor classes details. This commit includes connection to jdbc and its properties are configurable in resources/politicalparty.properties

--- src/com/politicalforum/providers/PoliticalPartyDAOServicesProvider.java
+++ src/com/politicalforum/providers/PoliticalPartyDAOServicesProvider.java
@@ -1,5 +1,27 @@
 package com.politicalforum.providers;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+import com.politicalforum.daoServices.PoliticalPartyDAOServices;
+import com.politicalforum.exceptions.ServicNotFoundException;
+
 public class PoliticalPartyDAOServicesProvider {
 
+	public static PoliticalPartyDAOServices getPoliticalPartyDAOServicesImplementor() throws ServicNotFoundException {
+		Properties prop = new Properties();
+		try {
+			prop.load(new FileReader(new File("resources/politicalparty.properties")));
+			return (PoliticalPartyDAOServices) Class.forName(prop.getProperty("politicalPartyDAOServices"))
+					.getConstructor().newInstance();
+		} catch (ReflectiveOperationException e) {
+			e.printStackTrace();
+			throw new ServicNotFoundException("Service Down!!", e);
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new ServicNotFoundException("Service Down", e);
+		}
+	}
 }
--- src/com/politicalforum/providers/PoliticalPartyServicesProvider.java
+++ src/com/politicalforum/providers/PoliticalPartyServicesProvider.java
@@ -1,8 +1,27 @@
 package com.politicalforum.providers;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+import com.politicalforum.exceptions.ServicNotFoundException;
+import com.politicalforum.services.PoliticalPartyServices;
+
 public class PoliticalPartyServicesProvider {
 
-	
-	
-	
+	public static PoliticalPartyServices getPoliticalPartyServiceImplementor() throws ServicNotFoundException {
+		Properties prop = new Properties();
+		try {
+			prop.load(new FileReader(new File("resources/politicalparty.properties")));
+			return (PoliticalPartyServices) Class.forName(prop.getProperty("politicalPartyServices")).getConstructor()
+					.newInstance();
+		} catch (ReflectiveOperationException e) {
+			e.printStackTrace();
+			throw new ServicNotFoundException("Service down!", e);
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new ServicNotFoundException("Service down!", e);
+		}
+	}
 }
