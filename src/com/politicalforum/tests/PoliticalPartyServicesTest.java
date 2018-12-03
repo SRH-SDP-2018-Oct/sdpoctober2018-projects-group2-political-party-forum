cc80a26aaf54cf7ea5fd34f811507b173d7b8566
Manul Aggarwal <manul.agg333@gmail.com>
2018-12-03 02:39:08 +0100

    [DEV][TEST]Added Test case to check the user registration process.

--- src/com/politicalforum/tests/PoliticalPartyServicesTest.java
+++ src/com/politicalforum/tests/PoliticalPartyServicesTest.java
@@ -1,5 +1,29 @@
 package com.politicalforum.tests;
 
+import org.hamcrest.core.StringStartsWith;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.politicalforum.exceptions.ServicNotFoundException;
+import com.politicalforum.services.PoliticalPartyServices;
+import com.politicalforum.services.PoliticalPartyServicesImplementation;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+
 public class PoliticalPartyServicesTest {
+
+	private static PoliticalPartyServices politicalServices;
+	
+	@BeforeClass
+	public static void setConnectionsBeforeAll() throws ServicNotFoundException {
+		politicalServices = new PoliticalPartyServicesImplementation();
+	}
+	
+	
+	@Test
+	public void testRegisterUserDetails() throws ServicNotFoundException {
+		assertThat(politicalServices.registerUserDetails("First", "Test", 30, "email2@id.com", "Female", "34124", false, "Delhi") , StringStartsWith.startsWith("U"));
+	}
 
 }
