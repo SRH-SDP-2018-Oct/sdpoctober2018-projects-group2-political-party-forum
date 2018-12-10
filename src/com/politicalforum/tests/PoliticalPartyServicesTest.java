ebea30098ec456b3bf2af8d588f6ba54c0295083
manzil94 <45242672+manzil94@users.noreply.github.com>
2018-12-10 15:45:33 +0100

    [DEV]Added Test Cases for Political User.

--- src/com/politicalforum/tests/PoliticalPartyServicesTest.java
+++ src/com/politicalforum/tests/PoliticalPartyServicesTest.java
@@ -4,11 +4,17 @@ import org.hamcrest.core.StringStartsWith;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.politicalforum.beans.User;
+import com.politicalforum.exceptions.GroupAlreadyExistException;
+import com.politicalforum.exceptions.InvalidCredentialsException;
 import com.politicalforum.exceptions.ServiceNotFoundException;
+import com.politicalforum.exceptions.UserAlreadyExistsException;
 import com.politicalforum.services.PoliticalPartyServices;
 import com.politicalforum.services.PoliticalPartyServicesImplementation;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
 
 import java.sql.SQLException;
 
@@ -16,15 +22,40 @@ import java.sql.SQLException;
 public class PoliticalPartyServicesTest {
 
 	private static PoliticalPartyServices politicalServices;
-	
+	private static User generalUser, politicalUser;
 	@BeforeClass
 	public static void setConnectionsBeforeAll() throws ServiceNotFoundException {
 		politicalServices = new PoliticalPartyServicesImplementation();
 	}
-		
+	
 	@Test
-	public void testRegisterUserDetails() throws ServiceNotFoundException, SQLException {
-	//	assertThat(politicalServices.registerUserDetails("abc", "def", 25, "nas@h.com", "Male", "123456", true, "Delhi", "abcd").getUserId(), StringStartsWith.startsWith("U"));
+	public void testRegisterUserDetails() throws ServiceNotFoundException, UserAlreadyExistsException {
+		generalUser = politicalServices.registerUserDetails("asd", "cc", 25, "ak1@ada", "M", "17723", false, "aaa", "abc");
+		assertThat(generalUser.getUserId(), StringStartsWith.startsWith("U"));
 	}
-
+    @Test
+    public void testRegisterPoliticalUserDetails() throws ServiceNotFoundException, UserAlreadyExistsException {
+    	politicalUser = politicalServices.registerPoliticalUserDetails("mm", "bal", 25, "ac@hj", "M", "1245", false, "kol", "qweruz");
+    	assertThat(politicalUser.getUserId(), StringStartsWith.startsWith("P"));
+    }
+    @Test
+    public void testlogin() throws InvalidCredentialsException {
+    	assertThat(politicalServices.login("ak1@ada", "abc").getUserId(), StringStartsWith.startsWith("U")); 
+    }
+    @Test
+    public void testcreateGroup() throws GroupAlreadyExistException {
+    	
+    	assertNull(politicalServices.createGroup("bjp", "the party", generalUser));
+        assertFalse(politicalServices.createGroup("bjp", "the party", politicalUser).getGroups().isEmpty());
+    }
+    @Test
+    public void testbrowseGroup() {
+    	assertFalse(politicalServices.browseGroups().isEmpty());
+    }
+    @Test
+    public void testcheckIfGroupExistsWithSimilarNames() {
+    	assertFalse(politicalServices.checkIfGroupExistsWithSimilarNames("bjp").isEmpty()); 
+    }
+    }
 }
+
